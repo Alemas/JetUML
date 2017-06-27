9c2b779442a64986097feee442b09e45f45ed4a8
Alemas <mateusreckziegel@gmail.com>
2017-06-26 22:34:03 -0300

    Description Tool Bar has proper JtextPanes for each node field

--- src/ca/mcgill/cs/stg/jetuml/framework/DescriptionToolBar.java
+++ src/ca/mcgill/cs/stg/jetuml/framework/DescriptionToolBar.java
@@ -1,14 +1,19 @@
 package ca.mcgill.cs.stg.jetuml.framework;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.LayoutManager;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
 
 import ca.mcgill.cs.stg.jetuml.graph.GraphElement;
 import ca.mcgill.cs.stg.jetuml.graph.Node;
@@ -17,12 +22,14 @@ public class DescriptionToolBar extends JPanel {
 
 	private static DescriptionToolBar instance = null;
 	private static JPanel descriptionPanel;
-	private static final int WIDTH = 40;
+	private static final int TOP_SPACING = 20;
 	private static final int SPACING = 5;
 	private static final int FONT_SIZE = 14;
-	private static JTextPane textPaneName;
-	private static JTextPane textPaneDescription;
-	private static JTextPane textPaneMethods;
+	private static final int LABEL_HEIGHT = 20;
+	private static final int LABEL_WIDTH = 60;
+	private static JTextPane textPaneName = new JTextPane();
+	private static JTextPane textPaneAttributes = new JTextPane();
+	private static JTextPane textPaneMethods = new JTextPane();
 
 	private DescriptionToolBar() {
 		super();
@@ -30,13 +37,21 @@ public class DescriptionToolBar extends JPanel {
 		descriptionPanel = new JPanel();
 		descriptionPanel.setLayout(new BoxLayout(descriptionPanel, BoxLayout.Y_AXIS));
 		
-		JLabel lblDescription = new JLabel("Description");
-		setFontSize(lblDescription, FONT_SIZE);
-		descriptionPanel.add(lblDescription);
+		addVerticalSpacing(TOP_SPACING);
 		
-		JLabel lblName = new JLabel("Name");
-		setFontSize(lblName, FONT_SIZE);
-		descriptionPanel.add(lblName);
+		addLabel("Name:");
+		addVerticalSpacing(SPACING);
+		addTextPane(textPaneName, new Dimension(200, 25));
+		addVerticalSpacing(SPACING);
+		
+		addLabel("Attributes:");
+		addVerticalSpacing(SPACING);
+		addTextPane(textPaneAttributes, new Dimension(200, 300));
+		addVerticalSpacing(SPACING);
+		
+		addLabel("Methods:");
+		addVerticalSpacing(SPACING);
+		addTextPane(textPaneMethods, new Dimension(200, 300));
 		
 	}
 	
@@ -44,11 +59,32 @@ public class DescriptionToolBar extends JPanel {
 		lbl.setFont(new Font(lbl.getFont().getFontName(), lbl.getFont().getStyle(), fontSize));
 	}
 	
+	private static void addVerticalSpacing(int spacing) {
+		Component space = Box.createRigidArea(new Dimension(0, spacing));
+		descriptionPanel.add(space);
+	}
+	
+	private static void addLabel(String text){
+		JLabel lbl = new JLabel(text);
+		setFontSize(lbl, FONT_SIZE);
+		descriptionPanel.add(lbl);
+	}
+	
+	private static void addTextPane(JTextPane tp, Dimension dimension) {
+		tp.setPreferredSize(dimension);
+		JScrollPane stp = new JScrollPane(tp, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		descriptionPanel.add(stp, BorderLayout.WEST);
+	}
+	
+	private static void addTextPane(JTextPane textPane) {
+		
+	}
+	
 	public static DescriptionToolBar getInstance() {
 		if (instance == null) {
 			instance = new DescriptionToolBar();
 			
-			instance.add(descriptionPanel, BorderLayout.NORTH);
+			instance.add(descriptionPanel);
 		}
 		return instance;
 	}
@@ -61,7 +97,7 @@ public class DescriptionToolBar extends JPanel {
 		if (instance == null) {
 			return;
 		}
-		instance.hide();
+		instance.setVisible(false);
 	}
 	
 	public static void clearDescription() {
